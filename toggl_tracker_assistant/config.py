b9025ab1dc58406285db78b046bca44532338f81
Oz <56094551+ozRnDs@users.noreply.github.com>
2024-10-13 23:44:11 +0200

    feat(AppConfig): Add the ability to save the configuration to the .env

--- toggl_tracker_assistant/config.py
+++ toggl_tracker_assistant/config.py
@@ -9,3 +9,16 @@ class AppConfig(BaseSettings):
     class Config:
         env_file = ".env"  # Specify the environment file to load variables from
         env_file_encoding = "utf-8"
+
+    def save_to_env(self):
+        """
+        Save the current status of the object to the .env file.
+        """
+        env_data = []
+        for field, value in self.model_dump().items():
+            if isinstance(value, list):
+                value = str(value).replace("'",'"')
+            env_data.append(f"{field}={value}")
+        
+        with open(self.Config.env_file, "w", encoding=self.Config.env_file_encoding) as f:
+            f.write("\n".join(env_data))
