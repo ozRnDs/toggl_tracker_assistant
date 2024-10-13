221718b1657cd42d52e3cc5ad1e5ebdf5c2daf77
Oz <56094551+ozRnDs@users.noreply.github.com>
2024-10-13 23:23:07 +0200

    feat(AppConfig): Add PROJECTS_LIST for the user's enabled projects

--- toggl_tracker_assistant/config.py
+++ toggl_tracker_assistant/config.py
@@ -1,9 +1,10 @@
 from pydantic_settings import BaseSettings
-
+from typing import List
 
 class AppConfig(BaseSettings):
     TOGGL_WORKSPACE: str
     TOGGL_API_KEY: str
+    PROJECTS_LIST: List[str]
 
     class Config:
         env_file = ".env"  # Specify the environment file to load variables from
